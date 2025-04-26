24caa702505e13bc7ca3fa9ff221de1afdf1da01
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-04-26 14:19:55 +0330

    Add add card functionality

--- js.js
+++ js.js
@@ -21,3 +21,49 @@ function findIndexById(iterable, id)
         index++;
     }
 }
+
+function addBookToDom(book) {
+    const folder = document.querySelector('.card-folder');
+    const card = document.createElement('div');
+    card.classList.add('card-item');
+
+    const title  = document.createElement('p');
+    const author = document.createElement('p');
+    const pages  = document.createElement('p');
+    const isRead = document.createElement('p');
+
+    title .classList.add('book-title');
+    author.classList.add('book-author');
+    pages .classList.add('book-pages');
+    isRead.classList.add('book-isRead');
+
+    title .textContent = book.title;
+    author.textContent = book.author;
+    pages .textContent = book.pages;
+    isRead.textContent = `Is Read: ${book.isRead}`;
+
+    const toggleIsRead = document.createElement('button');
+    toggleIsRead.textContent = 'Toggle Read';
+    toggleIsRead.type = 'button';
+    toggleIsRead.classList.add('toggle-read-button');
+    
+    const deleteCard   = document.createElement('button');
+    deleteCard.textContent = '---Remove---';
+    deleteCard.type = 'button';
+    deleteCard.classList.add('delete-card');
+
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(isRead);
+    card.appendChild(toggleIsRead);
+    card.appendChild(deleteCard);
+
+
+    folder.appendChild(card);
+}
+
+addBookToDom(new Book('Best Book', 'Adam', '617', true));
+addBookToDom(new Book('2nd Best Book', 'Eve', '716', false));
+addBookToDom(new Book('3rd Best Book', 'Isaac', '213', true));
