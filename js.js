fb9626596afc4ebb7b62d7ff626245b230d9df99
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-04-30 16:01:02 +0330

    Change Book constructor to class

--- js.js
+++ js.js
@@ -1,25 +1,17 @@
 const myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-    this.title  = title;
-    this.author = author;
-    this.pages  = pages;
-    this.isRead = isRead;
-    this.id     = crypto.randomUUID();
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.title  = title;
+        this.author = author;
+        this.pages  = pages;
+        this.isRead = isRead;
+        this.id     = crypto.randomUUID();
+    }
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
     myLibrary.push(new Book(title, author, pages, isRead));
-}
-
-function findIndexById(iterable, id)
-{
-    let index = 0;
-    for (const element of iterable) {
-        if (element.id === id)
-            return index;
-        index++;
-    }
 }
 
 function createCard(book) {
